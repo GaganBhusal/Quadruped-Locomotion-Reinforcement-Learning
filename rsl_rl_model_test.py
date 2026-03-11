fa5ab523224817817671d445b764d148d0e544ce
Gagan Bhusal <gaganbhusal20@gmail.com>
2026-03-11 11:50:33 +0545

    Add: File to test the model

---
+++ rsl_rl_model_test.py
@@ -0,0 +1,56 @@
+import argparse
+import os
+import pickle
+import torch
+import genesis as gs
+from rsl_rl.runners import OnPolicyRunner
+from walk_env_batch_terrain import WalkENV
+import time
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="go2_rsl")
+    parser.add_argument("--ckpt", type=int, default=2000)
+    parser.add_argument("--device", type=str, default="cuda")
+    args = parser.parse_args()
+
+    gs.init(backend=gs.gpu if args.device == "cuda" else gs.cpu, logging_level="warning")
+
+    log_dir = f"logs/{args.exp_name}"
+
+    with open(os.path.join(log_dir, "cfgs.pkl"), "rb") as f:
+        train_cfg = pickle.load(f)
+
+
+    env = WalkENV(
+        num_envs=1,
+        render=True,
+        device=args.device)
+
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=args.device)
+    resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
+    print(f"Loading checkpoint from {resume_path}")
+    runner.load(resume_path)
+
+    policy = runner.get_inference_policy(device=args.device)
+
+    obs, _ = env.reset()
+    obs = obs.to(args.device)
+
+    with torch.no_grad():
+        while True:
+            actions = policy(obs)
+            obs, reward, done, info = env.step(actions)
+            # torch.cuda.synchronize()
+            print(done)
+            print()
+            
+            # obs = obs.to(args.device)
+            # if done[0]:
+            #     print("Episode ended !!!!.")
+            #     obs, _ = env.reset()
+            #     obs = obs.to(args.device)
+
+if __name__ == "__main__":
+    main()
